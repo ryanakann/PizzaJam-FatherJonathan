6c54a66b8292f9b40c23cec7d18f2f86d6d8c0e7
Unknown <ryanakann@gmail.com>
2018-10-27 15:43:51 -0400

    First Level Nearly Finished (intro)

---
+++ Father Johnathan/Assets/ComeOnIn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComeOnIn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        EventManager.StartListening(EventType.ObtainCheese, IThinkYoullLikeIt);
+	}
+	
+    void IThinkYoullLikeIt () {
+        Invoke("Wait", 1.5f);
+    }
+
+    void Wait () {
+        GetComponent<AudioSource>().Play();
+    }
+}
