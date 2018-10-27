e249b0033f830590f20b2aad4510ee0a39764ab1
Unknown <ryanakann@gmail.com>
2018-10-26 22:31:52 -0400

    Maybe?

---
+++ Father Johnathan/Assets/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour {
+
+    NavMeshAgent agent;
+    Transform target;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindWithTag("Player").transform;
+        agent.SetDestination(target.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //agent.CalculatePath(target.position, 
+	}
+}
---
+++ Father Johnathan/Assets/LookAtCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+
+    private Transform cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(new Vector3(cam.position.x, transform.position.y, cam.position.z), Vector3.up);
+	}
+}
