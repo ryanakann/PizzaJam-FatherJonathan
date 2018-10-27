41449e37f7762a5d2c4e3c9c1be8aedbc6c2f2c3
Unknown <ryanakann@gmail.com>
2018-10-27 05:02:10 -0400

    Auto stash before merge of "Ryan" and "origin/master"

---
+++ Father Johnathan/Assets/UILevelTransition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UILevelTransition : MonoBehaviour {
+
+    public static UILevelTransition instance;
+
+    RawImage image;
+    public float fadeTime = 2f;
+
+    void Start () {
+        instance = this;
+
+        image = GetComponent<RawImage>();
+        image.color = new Color(1f, 0f, 0f, 0f);
+
+        DontDestroyOnLoad(transform.parent);
+    }
+
+    public void OnTransition () {
+        StartCoroutine(Fade());
+    }
+
+    IEnumerator Fade () {
+        while (image.color.a < 1f) {
+            image.color += new Color(0f, 0f, 0f, Time.deltaTime / fadeTime);
+            yield return new WaitForEndOfFrame();
+        }
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+
+        while (image.color.a > 0f) {
+            image.color -= new Color(0f, 0f, 0f, Time.deltaTime / fadeTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
