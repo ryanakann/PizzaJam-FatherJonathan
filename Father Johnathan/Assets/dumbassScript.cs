9578e89a86d2cc654a7d6e8638bd745b26f1ae9b
SimonMarkWhittle <smw2ed@virginia.edu>
2018-10-27 03:30:09 -0400

    thos dors open

---
+++ Father Johnathan/Assets/dumbassScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dumbassScript : MonoBehaviour {
+
+    public GameObject doors;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = doors.GetComponent<Animator>();
+        anim.SetTrigger("Toggle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
