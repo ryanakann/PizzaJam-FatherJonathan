af774d5feb87e787fb5878b1272293db294ebfe8
SimonMarkWhittle <smw2ed@virginia.edu>
2018-10-28 12:39:30 -0400

    new ending scene

---
+++ Father Johnathan/Assets/CreditTextFade.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CreditTextFade : MonoBehaviour {
+
+    public static CreditTextFade instance;
+
+    TextMeshProUGUI text;
+
+    public float waitTime = 5f;
+    public float fadeTime = 2f;
+    public float holdTime = 2f;
+
+    float elapsedTime = 0f;
+
+    void Start() {
+        instance = this;
+
+        text = GetComponent<TextMeshProUGUI>();
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0f);
+
+        StartCoroutine(Fadein(fadeTime, 0));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    IEnumerator Fadein(float fadeTime, int progress) {
+        if (progress == 0) {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, 0f);
+        }
+
+        if (progress == 0) {
+            yield return new WaitForSeconds(waitTime);
+        }
+
+
+        while (text.color.a < 1f && elapsedTime < fadeTime) {
+            elapsedTime += Time.deltaTime;
+            text.color += new Color(0f, 0f, 0f, Time.deltaTime / fadeTime);
+            yield return new WaitForEndOfFrame();
+        }
+
+        while (elapsedTime > fadeTime && elapsedTime < fadeTime + holdTime) {
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        while (text.color.a > 0f && elapsedTime > (fadeTime + holdTime) && elapsedTime < (2 * fadeTime) + holdTime) {
+            elapsedTime += Time.deltaTime;
+            text.color -= new Color(0f, 0f, 0f, Time.deltaTime / fadeTime);
+            yield return new WaitForEndOfFrame();
+        }
+
+        while (text.color.a > 0f) {
+            text.color -= new Color(0f, 0f, 0f, Time.deltaTime / fadeTime);
+            yield return new WaitForEndOfFrame();
+        }
+
+
+    }
+}
---
+++ Father Johnathan/Assets/Scripts/CreditsFadeIn.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsFadeIn : MonoBehaviour {
+
+    public static CreditsFadeIn instance;
+
+    RawImage image;
+    public float fadeTime = 2f;
+    public float waitTime = 5f;
+
+	void Start () {
+        instance = this;
+
+        image = GetComponent<RawImage>();
+        // image.color = new Color(image.color.r, image.color.g, image.color.b, 1f);
+
+        StartCoroutine(Fadein(fadeTime, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Fadein(float fadeTime, int progress) {
+        if (progress == 0) {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 1f);
+        }
+
+        while (image.color.a < 1f) {
+            image.color -= new Color(0f, 0f, 0f, Time.deltaTime / fadeTime);
+            yield return new WaitForEndOfFrame();
+        }
+
+        if (progress == 0) {
+            yield return new WaitForSeconds(waitTime);
+        }
+
+
+        while (image.color.a > 0f) {
+            image.color -= new Color(0f, 0f, 0f, Time.deltaTime / fadeTime);
+            yield return new WaitForEndOfFrame();
+        }
+
+
+    }
+}
