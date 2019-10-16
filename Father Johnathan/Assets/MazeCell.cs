a7f1019ad168cdd9db234f6d291a8787300b67b0
ryanakann <ryanakann@gmail.com>
2019-10-16 18:01:52 -0400

    Maze

---
+++ Father Johnathan/Assets/MazeCell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCell : MonoBehaviour
+{
+
+	public GameObject wallL;
+	public GameObject wallR;
+	public GameObject wallU;
+	public GameObject wallD;
+}
